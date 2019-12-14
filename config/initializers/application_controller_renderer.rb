e3ed57f2fa67dec31dae5862ad0e2cf3475d4ab3
wakasugishiho <athkrmkc@gmail.com>
2019-12-14 06:32:36 +0000

    [complete]user-system

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '647abfc8eb0b69e519463dfd1298ac646881ce60fb72da64afa756e88998bf338b877360da87ad1c9d728d94202bb05cd26e65c8f139756afb45e9f95209eb1f'
