4a5787225d5e09388e7ba14828817b29af53c42b
wakasugishiho <athkrmkc@gmail.com>
2019-12-16 03:33:16 +0000

    [complete]post-system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/index'
 	devise_for :users, :controllers => {
    		:registrations => 'users/registrations',
    		:sessions => 'users/sessions',
@@ -8,5 +11,6 @@ Rails.application.routes.draw do
   	root to: 'homes#top'
 
   resources :users, only: [:show, :edit, :update]
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_033355) do
+ActiveRecord::Schema.define(version: 2019_12_15_021424) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "title"
+    t.string "image_id"
+    t.text "body"
+    t.boolean "flag"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
