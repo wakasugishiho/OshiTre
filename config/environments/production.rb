2fe38e32709166a48601b26b453e9dca02ab916c
wakasugishiho <athkrmkc@gmail.com>
2019-12-25 07:07:53 +0000

    [fix]devise-system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://oshitre.com/', /http:\/\/oshitre.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://13.112.9.159/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
